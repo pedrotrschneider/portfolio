435650e396679a13b96cbfe24b1aac5a2c9d78af
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2022-12-26 20:10:02 -0300

    🎉 Add initial portfolio design

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Code for the nav button toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open')
+    })
+});
+
+// Code for the cool slide in animations
+const intersectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => intersectionObserver.observe(element));
